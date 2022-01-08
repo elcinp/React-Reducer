0ecd34c50d7db60502474bc52db04a28c7cdb3e4
elcinp <elcinpars1@gmail.com>
2022-01-08 09:22:47 +0300

    v7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import useStateExample from './useStateExample';
 
 function App() {
   return (
     <div className="App">
       <h1>Resume  cv  </h1>
+      <useStateExample/>
     </div>
   );
 }
